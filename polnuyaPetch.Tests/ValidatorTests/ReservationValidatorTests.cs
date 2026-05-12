4a0baddf0ac072245eb9d3f54594e2f3a490a1fd
ABSOLUTE123456 <aleksandrkruglov061@gmail.com>
2026-05-12 12:23:03 +0300

    test: extend tests and add LR14 report

--- polnuyaPetch.Tests/ValidatorTests/ReservationValidatorTests.cs
+++ polnuyaPetch.Tests/ValidatorTests/ReservationValidatorTests.cs
@@ -10,45 +10,40 @@ namespace polnuyaPetch.Tests.ValidatorTests
         [TestMethod]
         public void Validate_NameEmpty_ReturnsError()
         {
-            
             var res = new Reservation { Name = "", Email = "test@mail.ru", Guests = 2 };
-
-            
             var error = ReservationValidator.Validate(res);
-
-           
             Assert.IsNotNull(error);
         }
 
         [TestMethod]
         public void Validate_NameTooLong_ReturnsError()
         {
-           
             var longName = new string('a', 51);
             var res = new Reservation { Name = longName, Email = "test@mail.ru", Guests = 2 };
-
-            
             var error = ReservationValidator.Validate(res);
+            Assert.IsNotNull(error);
+        }
 
-           
+        [TestMethod]
+        public void Validate_DescriptionTooLong_ReturnsError()
+        {
+            // Тест из Шага 11: проверка слишком длинного сообщения
+            var longDesc = new string('b', 501);
+            var res = new Reservation { Name = "Ок", Email = "test@mail.ru", Message = longDesc };
+            var error = ReservationValidator.Validate(res);
             Assert.IsNotNull(error);
         }
 
         [TestMethod]
         public void Validate_ValidReservation_ReturnsNull()
         {
-            
             var res = new Reservation
             {
                 Name = "Иван",
                 Email = "ivan@mail.ru",
                 Guests = 4
             };
-
-           
             var error = ReservationValidator.Validate(res);
-
-            
             Assert.IsNull(error);
         }
     }
